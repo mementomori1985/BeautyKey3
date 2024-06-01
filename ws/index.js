e424664db88ab434ac4ded4116ae0381bf6f9bdd
Fabio Neves dos Santos <97970863+mementomori1985@users.noreply.github.com>
2024-06-01 13:40:56 -0300

    Update index.js

--- ws/index.js
+++ ws/index.js
@@ -1,17 +1,21 @@
 //configurando o web service e as rotas 
-const express = require ('express');
-const app = express ();
-const morgan = require ('morgan');
-const cors = require ('cors');
+const express = require('express');
+const app = express();
+const morgan = require('morgan');
+const busboy = require('connect-busboy');
+const busboyBodyParser = require('busboy-body-parser');
+const cors = require('cors');
 
 require ('./database');
 
 //configurando o get e o set da aplicação
-app.use (morgan('dev'));
+app.use(morgan('dev'));
+app.use(busboy());
+app.use(busboyBodyParser());
 app.use(express.json());
 app.use(cors());
 
-app.set ('port', 27017);
+app.set ('port', 8000);
 
 //rotas da aplicação
 app.use('/salao', require('./src/routes/salao.routes'));
@@ -25,4 +29,3 @@ app.listen (app.get('port'), () => {
     console.log (`Web Service escuta na porta ${app.get('port')}`)
 });
  
-
