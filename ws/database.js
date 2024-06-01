6cebc0a7dbd682f26f1bcf89992ecbe825236e69
Fabio Neves dos Santos <97970863+mementomori1985@users.noreply.github.com>
2024-06-01 13:40:03 -0300

    Update database.js

--- ws/database.js
+++ ws/database.js
@@ -1,14 +1,19 @@
 //chamando o mongoose
-const mongoose = require ('mongoose');
-const URI = 'mongodb+srv://beauty:projeto2420@cluster1.xhbtagc.mongodb.net/'; 
+const mongoose = require('mongoose');
 
+const URI = 'mongodb+srv://beauty:projeto2420@cluster1.xhbtagc.mongodb.net/?retryWrites=true&w=majority&appName=Cluster1';
 
 const env = process.env.NODE_ENV || 'dev';
-let options = {};
+
+let options = {
+    useNewUrlParser: true,
+    useFindAndModify: false,
+    useCreateIndex: true,
+    useUnifiedTopology: true
+};
 
 //configurando o promisse do BD
 mongoose.connect(URI)
 .then (() => console.log('Banco de dados conectado!'))
 .catch((error) => console.log('Erro ao conectar ao banco de dados:',error));
-
 
